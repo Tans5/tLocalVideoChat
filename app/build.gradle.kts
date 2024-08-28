4d0977e23558b5d3a143aa658a5effbf159de036
tanpengcheng <tanpengcheng@inke.cn>
2024-08-28 16:38:36 +0800

    Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleKsp)
+}
+
+android {
+    namespace = "com.tans.tlocalvideochat"
+    compileSdk = properties["ANDROID_COMPILE_SDK"].toString().toInt()
+
+    defaultConfig {
+        applicationId = "com.tans.tlocalvideochat"
+        minSdk = properties["ANDROID_MIN_SDK"].toString().toInt()
+        targetSdk = properties["ANDROID_TARGET_SDK"].toString().toInt()
+        versionCode = properties["VERSION_CODE"].toString().toInt()
+        versionName = properties["VERSION_NAME"].toString()
+
+        setProperty("archivesBaseName", "tlocalvideochat-${properties["VERSION_NAME"].toString()}")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // android
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // coroutine
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.core.jvm)
+    implementation(libs.coroutines.android)
+
+    // moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.moshi.adapters)
+    ksp(libs.moshi.kotlin.codegen)
+
+    // tans5
+    implementation(libs.tuiutils)
+
+    // netty
+    implementation(libs.netty)
+
+    // webrtc
+    implementation(libs.webrtc)
+}
